222b2a2b0480ddb823d8259967171abe61290425
Alper <alperuluses35@gmail.com>
2025-02-05 17:41:11 +0300

    apiUrlChange

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://chatmeal-production-8b07.up.railway.app/api', // Development API URL
+    apiUrl: 'https://chatmeal.onrender.com/api', // Development API URL
     socketUrl: 'https://chatmeal-production-8b07.up.railway.app:3001' // Development Socket URL
   };
