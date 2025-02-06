8a21d745a8cd11bc0ef2d24294ee169f9d3ca373
Alper <alperuluses35@gmail.com>
2025-02-07 01:42:11 +0300

    socket server

---
+++ socket/src/socket.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const { initializeSocket } = require('./services/socketService');
+
+const server = http.createServer();
+initializeSocket(server);
+
+const SOCKET_PORT =  process.env.PORT || 3000;
+server.listen(SOCKET_PORT, () => {
+    console.log(`Socket.io ${SOCKET_PORT} portunda çalışıyor`);
+});
+
+
