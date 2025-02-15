dea96d9de175951b44bfdd000c4bfded97642255
Alper <alperuluses35@gmail.com>
2025-02-15 12:46:51 +0300

    Error log added for login

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,32 +1,43 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const User = require("../models/Users");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const SECRET_KEY = process.env.JWT_SECRET || "chatAruSecret";
 // Kullanıcı kaydı
 exports.register = (req, res) => {
-    const { username,email, password } = req.body;
-    User.findByUsername(email, (err, user) => {
-        if (user) return res.status(400).json({ message: "Kullanıcı zaten var" });
+  const { username, email, password } = req.body;
+  User.findByUsername(email, (err, user) => {
+    if (user) return res.status(400).json({ message: "Kullanıcı zaten var" });
 
-        User.register(username,email, password, (err, result) => {
-            if (err) return res.status(500).json({ message: "Kayıt hatası" });
-            res.status(201).json({ message: "Kayıt başarılı" });
-        });
+    User.register(username, email, password, (err, result) => {
+      if (err) return res.status(500).json({ message: "Kayıt hatası" });
+      res.status(201).json({ message: "Kayıt başarılı" });
     });
-}
+  });
+};
 
 exports.login = (req, res) => {
-    const { email, password } = req.body;
-    
-    User.findByUsername(email, (err, user) => {
-        if (!user) return res.status(400).json({ message: "Kullanıcı bulunamadı" });
+  const { email, password } = req.body;
 
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (!isMatch) return res.status(400).json({ message: "Şifre yanlış" });
+  User.findByUsername(email, (err, user) => {
+    if (err) {
+      console.error("Veritabanı hatası:", err);
+      return res.status(500).json({ message: "Bir hata oluştu." });
+    }
 
-            const token = jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '24h' });
-            res.json({ token, username: user.username });
-        });
+    if (!user) {
+      return res.status(400).json({ message: "Kullanıcı bulunamadı" });
+    }
+
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (!isMatch) return res.status(400).json({ message: "Şifre yanlış" });
+
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        SECRET_KEY,
+        { expiresIn: "24h" }
+      );
+      res.json({ token, username: user.username });
     });
-}
+  });
+};
