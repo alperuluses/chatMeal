a1e9f9562c849848ff29ecb7d4164b7ac8c0af99
Alper <alperuluses35@gmail.com>
2025-02-16 15:52:30 +0300

    Pool connection added

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,24 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host:  process.env.DB_HOST,
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port:process.env.DB_PORT,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("MySQL bağlantısı başarılı!");
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Veritabanı bağlantı hatası:', err);
+    } else {
+        console.log('MySQL bağlantısı başarılı!');
+        connection.release(); // Bağlantıyı hemen bırakıyoruz, sadece testi yaptık
+    }
 });
 
-module.exports = connection;
+module.exports = pool;
