8e6c97e4de1bdafd7b5116bb27c55e2f372ebde3
Alper <alperuluses35@gmail.com>
2025-02-21 01:36:49 +0300

    Backend cloud render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://chatmeal-v1.up.railway.app/api', // Prod API URL
-  socketUrl: 'https://chatmeal-socket.up.railway.app' // Prod Socket URL
+  apiUrl: 'https://chatmeal.onrender.com/api', // Prod API URL
+  socketUrl: 'https://socket-y819.onrender.com' // Prod Socket URL
 };
